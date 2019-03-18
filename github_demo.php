c4d20f1619ec96866b4a1802a633bd7bcfa78822
Seema Nagar <seemanagarps@gmail.com>
2019-03-18 22:38:40 +0530

    updates

--- github_demo.php
+++ github_demo.php
@@ -1,1 +1,23 @@
 <p>This is for gitgub demo </p>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+
+Github Commands
+git init
+
+git add .
+
+git commit -m "First commit"
+
+git remote add origin remote repository URL
+
+git remote -v
+
+git push origin master
